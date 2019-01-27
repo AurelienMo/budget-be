e7d3b5feaeb04e1d340bdf2748cd3cd47efdac9c
Aurelien Morvan <morvan.aurelien@gmail.com>
2019-01-27 23:05:21 +0100

    [Account/Confirm Registration] Implement Persister to save confirm account

--- src/Actions/Account/ConfirmAccount.php
+++ src/Actions/Account/ConfirmAccount.php
@@ -14,8 +14,10 @@ declare(strict_types=1);
 namespace App\Actions\Account;
 
 use App\Actions\AbstractApiAction;
+use App\Domain\Account\ConfirmRegistration\Persister;
 use App\Domain\Account\ConfirmRegistration\RequestResolver;
 use App\Domain\Common\Exceptions\ValidatorException;
+use Doctrine\ORM\NonUniqueResultException;
 use Swagger\Annotations as SWG;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -29,15 +31,21 @@ class ConfirmAccount extends AbstractApiAction
     /** @var RequestResolver */
     protected $resolver;
 
+    /** @var Persister */
+    protected $persister;
+
     /**
      * ConfirmAccount constructor.
      *
      * @param RequestResolver $resolver
+     * @param Persister       $persister
      */
     public function __construct(
-        RequestResolver $resolver
+        RequestResolver $resolver,
+        Persister $persister
     ) {
         $this->resolver = $resolver;
+        $this->persister = $persister;
     }
 
     /**
@@ -48,6 +56,7 @@ class ConfirmAccount extends AbstractApiAction
      * @return Response
      *
      * @throws ValidatorException
+     * @throws NonUniqueResultException
      *
      * @SWG\Response(
      *     response="200",
@@ -62,7 +71,8 @@ class ConfirmAccount extends AbstractApiAction
     public function confirm(Request $request)
     {
         $input = $this->resolver->resolve($request);
+        $output = $this->persister->save($input);
 
-        return $this->sendResponse();
+        return $this->sendResponse($output);
     }
 }
